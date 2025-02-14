fe96c317833c3a93a66895efcdea6161faecb15e
Aarav Goyal <gl.aarav@gmail.com>
2025-02-14 15:05:39 -0800

    Update Masterpiece.java

--- Masterpiece.java
+++ Masterpiece.java
@@ -1,3 +1,11 @@
+/*
+ * 2-14-2025
+ * Aarav Goyal
+ * Masterpiece.java
+ * 
+ * This program will draw a Flower and print "Flower" on the bottom.
+ */ 
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Font;
@@ -17,7 +25,8 @@ public class Masterpiece
         JFrame frame = new JFrame("Flower Masterpiece");
         frame.setSize(600, 500);
         frame.setLocation(200, 100);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setResizable(false);       
         MasterpiecePanel mpPanel = new MasterpiecePanel();
         frame.getContentPane().add(mpPanel);
         frame.setVisible(true);
@@ -28,7 +37,7 @@ class MasterpiecePanel extends JPanel
 {
     public MasterpiecePanel()
     {
-        setBackground(Color.WHITE);
+        setBackground(new Color(0, 166, 255));
     }
     
     public void paintComponent(Graphics g)
@@ -39,10 +48,12 @@ class MasterpiecePanel extends JPanel
     }
     
     public void drawFlower(Graphics g)
-    {
-        
+    {	
         g.setColor(new Color(34, 139, 34)); 
         g.fillRect(290, 200, 20, 150);
+        
+        g.setColor(new Color(151, 113, 16));
+      //  g.fillRect(
         
         g.setColor(new Color(0, 128, 0)); 
         int[] leafX1 = {280, 250, 290};
@@ -58,23 +69,25 @@ class MasterpiecePanel extends JPanel
         g.fillOval(300, 130, 50, 50); 
         g.fillOval(250, 180, 50, 50);
         g.fillOval(300, 180, 50, 50); 
+        g.fillArc(275, 130, 50, 50, 0, 180);
         
         g.setColor(Color.YELLOW);
         g.fillOval(275, 155, 50, 50);
-        
-        g.setColor(Color.PINK);
-        g.fillArc(275, 130, 50, 50, 0, 180);
-        
+       
         g.setColor(Color.BLACK);
-        g.drawLine(290, 200, 290, 150);
-        g.drawLine(310, 200, 310, 150);
+        g.drawLine(310, 203, 290, 157);
+        g.drawLine(290, 203, 310, 157);
+        g.drawLine(275, 180, 325, 180);
+        g.fillOval(295, 175, 10, 10); 
         
+        g.drawString("Flower", 280, 400);
         
+       
     }
     
     public void drawGrid(Graphics g)
     {
-        int size = 20;
+        int size = 10;
         g.setFont(new Font("serif", Font.PLAIN, 15));
         for(int x = 0; x <= 600; x += size)
         {
