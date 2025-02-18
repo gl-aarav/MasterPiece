cff7431cfe4874dc2b5bec1366c29060f0600c7e
gl-aarav <gl.aarav@gmail.com>
2025-02-18 09:42:01 -0800

    updates

--- Masterpiece.java
+++ Masterpiece.java
@@ -3,7 +3,7 @@
  * Aarav Goyal
  * Masterpiece.java
  * 
- * This program will draw a Flower and print "Flower" on the bottom.
+ * This program will draw three Flowers and print "Flower" on the bottom.
  */ 
 
 import java.awt.Color;
@@ -43,46 +43,73 @@ class MasterpiecePanel extends JPanel
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        drawGrid(g);
-        drawFlower(g);
+        //drawGrid(g);
+        drawFlower(g, 290, 200); 
+        drawFlower(g, 100, 250); 
+        drawFlower(g, 450, 230);
     }
     
-    public void drawFlower(Graphics g)
-    {	
+    public void drawFlower(Graphics g, int x, int y)
+    {   
+    	
+         
         g.setColor(new Color(34, 139, 34)); 
-        g.fillRect(290, 200, 20, 150);
+        g.fillRect(x, y, 20, 150);
         
         g.setColor(new Color(151, 113, 16));
-      //  g.fillRect(
+        g.fillRect(0, 350, 600, 150);
+        
+        g.setColor(new Color(34, 139, 34)); 
+        g.fillRect(0, 350, 600, 10);
+        
+        for (int i = 0; i < 600; i+=5)
+        {
+        	g.fillRect(i, 342, 2, 8);
+        }
         
         g.setColor(new Color(0, 128, 0)); 
-        int[] leafX1 = {280, 250, 290};
-        int[] leafY1 = {250, 280, 270};
+        int[] leafX1 = {x - 10, x - 40, x};
+        int[] leafY1 = {y + 50, y + 80, y + 70};
         g.fillPolygon(leafX1, leafY1, 3);
         
-        int[] leafX2 = {310, 350, 320};
-        int[] leafY2 = {270, 280, 250};
+        int[] leafX2 = {x + 20, x + 60, x + 30};
+        int[] leafY2 = {y + 70, y + 80, y + 50};
         g.fillPolygon(leafX2, leafY2, 3);
+        g.setColor(Color.RED);
+        g.fillArc(x - 20, y - 70, 60, 60, 0, 180); 
+        g.fillArc(x - 40, y - 50, 60, 60, 90, 180); 
+        g.fillArc(x + 2, y - 50, 60, 60, 270, 180); 
+        g.fillArc(x - 18, y - 30, 60, 60, 180, 180); 
         
         g.setColor(Color.PINK);
-        g.fillOval(250, 130, 50, 50); 
-        g.fillOval(300, 130, 50, 50); 
-        g.fillOval(250, 180, 50, 50);
-        g.fillOval(300, 180, 50, 50); 
-        g.fillArc(275, 130, 50, 50, 0, 180);
+        g.fillOval(x - 50, y - 80, 60, 60); 
+        g.fillOval(x + 10, y - 80, 60, 60); 
+        g.fillOval(x - 50, y - 20, 60, 60);
+        g.fillOval(x + 10, y - 20, 60, 60); 
         
         g.setColor(Color.YELLOW);
-        g.fillOval(275, 155, 50, 50);
+        g.fillOval(x - 15, y - 45, 50, 50);
        
         g.setColor(Color.BLACK);
-        g.drawLine(310, 203, 290, 157);
-        g.drawLine(290, 203, 310, 157);
-        g.drawLine(275, 180, 325, 180);
-        g.fillOval(295, 175, 10, 10); 
+        g.drawLine(x + 20, y + 3, x, y - 43);
+        g.drawLine(x, y + 3, x + 20, y - 43);
+        g.drawLine(x - 15, y - 20, x + 35, y - 20);
+        g.fillOval(x + 5, y - 25, 10, 10); 
         
-        g.drawString("Flower", 280, 400);
+        g.setColor(Color.WHITE);
+        g.fillRect(45,50,100,50);
+        g.fillOval(53,5,90,90);
+        g.fillOval(20,50,50,50);
+        g.fillOval(125,50,50,50);
         
-       
+        g.fillRect(445,50,100,50);
+        g.fillOval(453,5,90,90);
+        g.fillOval(420,50,50,50);
+        g.fillOval(525,50,50,50);
+        
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("serif", Font.PLAIN, 40));
+        g.drawString("Flower", x-210, 425);
     }
     
     public void drawGrid(Graphics g)
